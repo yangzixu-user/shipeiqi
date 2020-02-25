bb2bfa54e84e794d86e732c78a1ccce7ca29939b
yangzixu-user <2513591994@qq.com>
2020-02-25 22:57:25 +0800

    first commit version-1.20

--- src/java/lang/yzx.java
+++ src/java/lang/yzx.java
@@ -3,5 +3,12 @@ package java.lang;
 public class yzx {
     public static void main(String[] args) {
         System.out.println("hello!");
+        System.out.println("1111!");
+        System.out.println("22222!");
+        System.out.println("33333!");
+        System.out.println("44444!");
+        System.out.println("55555!");
+        System.out.println("55555!");
     }
+
 }
